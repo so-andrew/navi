6fe3df5557e9375fd2a1fd087728d0f3dccfc676
Andrew So <so.andrew0@gmail.com>
2022-10-12 01:24:33 -0400

    Updated about and readme

--- README.md
+++ README.md
@@ -1,4 +1,12 @@
 # NAVI
 A Discord bot I made to use in small personal servers for managing roles and providing various utilities.
 
-Work in progress...
+Current functionality includes: 
+
+- Displaying server/user information
+- Managing roles for members in order to access private channels
+- League of legends rank lookup and daily leaderboard
+- Twitch-style predictions with points betting
+- Bulk delete messages in a channel
+
+with more functionality to come.
--- commands/about.js
+++ commands/about.js
@@ -8,7 +8,7 @@ module.exports = {
 		const BotInfoEmbed = new EmbedBuilder()
 			.setColor(0x0ea9f4)
 			.setTitle('NAVI - Knowledge Navigator')
-			.setDescription('A Discord bot for managing small servers, made using the Discord.js library.\n\nCurrent functionality includes:\n\n- Displaying server/user information\n- Managing roles for members in order to access private channels\n- League of Legends rank lookup and daily leaderboard\n- Bulk deleting messages in a channel\n\nMore functionality coming soon.')
+			.setDescription('A Discord bot for managing small servers, made using the Discord.js library.\n\nCurrent functionality includes:\n\n- Displaying server/user information\n- Managing roles for members in order to access private channels\n- League of Legends rank lookup and daily leaderboard\n- Twitch-style predictions with points betting\n- Bulk deleting messages in a channel\n\nMore functionality coming soon.')
 			.addFields(
 				{ name: 'Created By', value: '<@197598081867448320>', inline: true },
 				{ name: 'GitHub Repository', value: 'https://github.com/so-andrew/navi/', inline: true },
